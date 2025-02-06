2183a0b80e9fd48615910f59c7a7e66453b96df3
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-02-06 12:29:34 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="lila">
+      <div class="kek"></div>
+      <div class="kek"></div>
+      <div class="kek"></div>
+    </div>
+  <div class="container">
+    <div class="zold">
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container{
+  background-color: yellow;
+  display: flex;
+  height: 400px;
+}
+.lila{
+  background-color: purple;
+  display: flex;
+  padding: 10px;
+
+}
+.kek{
+  background-color: aqua;
+  height: 30px;
+  width: 100px;
+  margin-right: 10px;
+}
+.zold{
+  background-color: green;
+}
+</style>
